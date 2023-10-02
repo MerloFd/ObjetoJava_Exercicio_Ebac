1870d540c0a70926537dcdf33bf7d6f83eb97969
Matheus Merlo <matheus.merlo02@gmail.com>
2023-10-02 20:46:53 -0300

    initial add projeto Objeto Carro v1

---
+++ src/Mustang_Gt_V8.java
@@ -0,0 +1,48 @@
+public class Mustang_Gt_V8 {
+
+    private String cor;
+    private String motor;
+    private String modelo;
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+
+    public String corMustang () {
+        getCor() = vermelho;
+        return getCor();
+    }
+
+    public String motorMustang () {
+        getMotor() = motor_V8;
+        return getMotor();
+    }
+
+    public String modeloMustang () {
+        getModelo() = Ford_mustang_gt;
+        return getModelo();
+    }
+}
